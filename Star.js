0be1d61b021f0c5e167a820f24b86f14653c300f
Aric Alves <aric.alves@yahoo.com>
2017-08-20 15:33:55 -0700

    attempting to implement intersection detection

--- Star.js
+++ Star.js
@@ -1,5 +1,5 @@
 function Star(y1, y2) {
-  this.x = random(0, 600);
+  this.x = random(0, width);
   this.y = random(y1, y2);
   this.width = random(0, 2);
   this.height = random(0, 2);
@@ -11,12 +11,13 @@ function Star(y1, y2) {
   }
 
   this.fall = function() {
-    if (this.y < 610) {
+    if (this.y < (height + 10)) {
       this.y += speed;
     }
   }
 
   this.isOffScreen = function() {
-    return (this.y > 600);
+    return (this.y > (height + 1));
   }
+
 }
--- sketch.js
+++ sketch.js
@@ -8,22 +8,26 @@ var paused = false;
 /********************/
 
 function setup() {
+
   noStroke();
   colorMode(HSB, 360, 100, 100, 1);
   createCanvas(600, 600);
   speed = 2;
   ship = new SpaceShip();
-  for (let i = 0; i < 250; i++) {
+
+  for (let i = 0; i < 300; i++) {
     starField.push(new Star(0, 600));
   }
 }
 
 function draw() {
+
   background(200, 100, 1, 1);
   renderStarField();
   ship.display();
   ship.move();
   ship.borderWrap();
+
   for (let i = 0; i < laserBeams.length; i++) {
     laserBeams[i].display();
     laserBeams[i].trajectory();
@@ -36,6 +40,14 @@ function draw() {
   for (let i = 0; i < meteorShower.length; i++) {
     meteorShower[i].display();
     meteorShower[i].fall();
+    for (let j = 0; j < meteorShower.length; j++) {
+      if (i != j && meteorShower[i].intersects(meteorShower[j])) {
+        console.log('intersects');
+        meteorShower.splice(j, 1);
+        i--;
+        j--;
+      }
+    }
     if (meteorShower[i].isOffScreen()) {
       meteorShower.splice(i, 1);
       i--;
@@ -51,14 +63,15 @@ function draw() {
     }
   }
 
-  let random;
   meteorShower.push(new Meteor);
 
 }
 
 function renderStarField() {
-  starField.push(new Star(0, 150));
+  starField.push(new Star(0, 1));
 }
+
+
 
 // Event Handlers //
 
